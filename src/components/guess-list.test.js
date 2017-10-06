39b7552953d72b1673e86b27d9acc96897400ecc
Matt Lee <matthewlee07@gmail.com>
2017-10-06 15:17:43 -0400

    guess list test working

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import GuessList from './guess-list';
 
 const guesses = [1, 2, 3];
@@ -7,5 +7,11 @@ const guesses = [1, 2, 3];
 describe('<GuessList /> Component', () => {
   it('Smoke test', () => {
     shallow(<GuessList guesses={guesses}/>);
-  })
+  });
+
+  it('Should render an array of guesses', () => {
+    const array = [1,2,3];
+    const wrapper = shallow(<GuessList guesses={array}/>);
+    expect(wrapper.find('li').length).toEqual(3);
+  });
 })
