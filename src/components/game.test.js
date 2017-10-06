66bc7f07e0da45f19d392ea8e3fc1d27b56acb7f
Matt Lee <matthewlee07@gmail.com>
2017-10-06 12:20:16 -0400

    4 successful shallow tests for Game

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Game from './game';
 
 describe('<Game /> Component', () => {
-it('Smoke test', () => {
+  const wrapper = shallow(<Game />);
+  it('Smoke test', () => {
     shallow(<Game />);
-  })
+  });
+  it('State of feedback = Make your guess! on load', () => {
+    expect(wrapper.state('feedback')).toEqual('Make your guess!');
+  });
+
+  it('If guess is an invalid number', () => {
+    wrapper.instance().guess('string');
+    expect(wrapper.state('feedback')).toEqual('Please enter a valid number');
+  });
+
+  it('If guess is a valid number', () => {
+    wrapper.instance().guess(50);
+    expect(wrapper.state('guesses')).toEqual([50]);
+  });
+
 })
