bf7c688eb7d7b519d83c479c1053d6415e013937
Matt Lee <matthewlee07@gmail.com>
2017-10-06 16:29:38 -0400

    successful TopNav testing

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -6,4 +6,18 @@ describe('<TopNav /> Component', () => {
   it('Smoke test', () => {
     shallow(<TopNav />);
   })
+
+  it('Fires the onWhat callback when the what button is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onInfo={callback}/>);
+    wrapper.find('.what').simulate('click', {preventDefault(){}});
+    expect(callback).toHaveBeenCalled();
+  })
+
+  it('Fires the onNewGame callback when the newGame button is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<TopNav onNewGame={callback}/>);
+    wrapper.find('.new').simulate('click', {preventDefault(){}});
+    expect(callback).toHaveBeenCalled();
+  })
 })
