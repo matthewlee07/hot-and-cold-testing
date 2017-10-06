2f647ac55e78027ff3612f53fe763e37cc429c69
Matt Lee <matthewlee07@gmail.com>
2017-10-06 16:24:10 -0400

    info-modal test passed

--- src/components/info-modal.test.js
+++ src/components/info-modal.test.js
@@ -6,4 +6,11 @@ describe('<InfoModal /> Component', () => {
   it('Smoke test', () => {
     shallow(<InfoModal />);
   })
+
+  it('Fires the onClose callback when the close button is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<InfoModal onClose={callback}/>);
+    wrapper.find('a').simulate('click', {preventDefault(){}});
+    expect(callback).toHaveBeenCalled();
+  })
 })
