8bb5d5fadc391853c9d832c214d9d0e75e07392e
Brian Besaw <bjbesaw@outlook.com>
2017-10-06 15:56:14 -0400

    added testing for header component

--- src/components/header.test.js
+++ src/components/header.test.js
@@ -6,4 +6,20 @@ describe('<Header /> Component', () => {
   it('Smoke test', () => {
     shallow(<Header />);
   })
+
+  it('Hides info modal on start', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.state('showInfoModal')).toEqual(false);
+  })
+
+  it('Toggles info modal on toggleInfoModal', () => {
+    const wrapper = shallow(<Header />);
+    wrapper.instance().toggleInfoModal()
+    expect(wrapper.state('showInfoModal')).toEqual(true);
+    wrapper.instance().toggleInfoModal()
+    expect(wrapper.state('showInfoModal')).toEqual(false);
+  })
+
+
+
 })
